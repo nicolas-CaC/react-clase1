9d0d18152d60d14d80d12002f1ca3a9356ac7b98
nicolas-CaC <nicolas.machicado@bue.edu.ar>
2023-03-01 22:07:30 -0300

    Clase 1 - Repaso

---
+++ defer.js
@@ -0,0 +1,16 @@
+document.getElementById('defer').innerText = "Soy un defer"
+
+let nombre = 'Mi nombre'
+
+nombre = 3
+nombre = true
+
+nombre = prompt('ingrese su nombre')
+
+alert('Soy un alerta')
+
+function lafuncion() {
+    document.open()
+    document.write(nombre)
+    document.close()
+}
---
+++ preload.js
@@ -0,0 +1,12 @@
+document.getElementById('preload').innerText = 'Soy el preload'
+
+const array = [1, 2, 3, 4];
+const objeto = { a: 1, b: 2, c: 3, d: 4 };
+
+console.log(array)
+console.log(objeto)
+
+console.table(array)
+console.table(objeto)
+console.warn('Guarda que puede venir un problema')
+console.error('Acá hubo un problema')
